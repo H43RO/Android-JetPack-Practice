293c39ebb0d195556617c64ed81ce610e2657224
H43RO <h2is1234@naver.com>
2020-11-24 23:40:32 +0900

    View Binding

--- ViewBinding/app/src/main/java/com/jeongdaeri/bottomnavwithfragments/HomeFragment.kt
+++ ViewBinding/app/src/main/java/com/jeongdaeri/bottomnavwithfragments/HomeFragment.kt
@@ -7,8 +7,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.jeongdaeri.bottomnavwithfragments.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    var count: Int = 0
+
+    // 뷰가 사라질 때 (메모리에서 날라갈 때) 같이 날리기 위해 따로 빼두기
+    private var fragmentHomeBinding: FragmentHomeBinding? = null
 
     companion object {
         const val TAG : String = "로그"
@@ -42,10 +48,27 @@ class HomeFragment : Fragment() {
 
         Log.d(TAG, "HomeFragment - onCreateView() called")
 
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
+//        val view = inflater.inflate(R.layout.fragment_home, container, false)
+//        return view
 
-        return view
+
+        // 자동으로 생성된 ViewBinding 가져옴
+        val binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        fragmentHomeBinding = binding
+
+        fragmentHomeBinding?.homeFragmentButton?.setOnClickListener {
+            Log.d(TAG, "HomeFragmentButton Clicked!")
+            count++
+            fragmentHomeBinding?.homeFragmentTextView?.text = "홈 : $count"
+        }
+
+        return fragmentHomeBinding!!.root
     }
 
-
+    override fun onDestroyView() {
+        // 메모리 릭 방지를 위해 ViewBinding 제거
+        fragmentHomeBinding = null
+        super.onDestroyView()
+    }
 }
--- ViewBinding/app/src/main/java/com/jeongdaeri/bottomnavwithfragments/MainActivity.kt
+++ ViewBinding/app/src/main/java/com/jeongdaeri/bottomnavwithfragments/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.jeongdaeri.bottomnavwithfragments.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,21 +16,28 @@ class MainActivity : AppCompatActivity() {
     private lateinit var profileFragment: ProfileFragment
 
     companion object {
-
         const val TAG: String = "로그"
-
     }
+
+    private var activityMainBinding : ActivityMainBinding? = null
 
     // 메모리에 올라갔을 때
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // 레이아웃과 연결
-        setContentView(R.layout.activity_main)
+        // 레이아웃과 연결 (기존 방식)
+        // setContentView(R.layout.activity_main)
+
+        // 자동으로 생성하는 바인딩 : MainActivity -> ActivityMainBinding
+        // 자동으로 완성된 액티비티 메인 바인딩 클래스 인스턴스를 가져옴
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        activityMainBinding = binding
+        // ViewBinding 과 연결
+        setContentView(activityMainBinding!!.root)
 
         Log.d(TAG, "MainActivity - onCreate() called")
 
-        bottom_nav.setOnNavigationItemSelectedListener(onBottomNavItemSelectedListener)
+        activityMainBinding?.bottomNav?.setOnNavigationItemSelectedListener(onBottomNavItemSelectedListener)
 
         homeFragment = HomeFragment.newInstance()
         supportFragmentManager.beginTransaction().add(R.id.fragments_frame, homeFragment).commit()
@@ -42,16 +50,25 @@ class MainActivity : AppCompatActivity() {
 
         when(it.itemId){
             R.id.menu_home -> {
+
+                activityMainBinding?.mainActivityTextView?.text = "MainActivity - 홈버튼 클릭!"
+
                 Log.d(TAG, "MainActivity - 홈버튼 클릭!")
                 homeFragment = HomeFragment.newInstance()
                 supportFragmentManager.beginTransaction().replace(R.id.fragments_frame, homeFragment).commit()
             }
             R.id.menu_ranking -> {
+
+                activityMainBinding?.mainActivityTextView?.text = "MainActivity - 랭킹버튼 클릭!"
+
                 Log.d(TAG, "MainActivity - 랭킹버튼 클릭!")
                 rankingFragment = RankingFragment.newInstance()
                 supportFragmentManager.beginTransaction().replace(R.id.fragments_frame, rankingFragment).commit()
             }
             R.id.menu_profile -> {
+
+                activityMainBinding?.mainActivityTextView?.text = "MainActivity - 프로필버튼 클릭!"
+
                 Log.d(TAG, "MainActivity - 프로필버튼 클릭!")
                 profileFragment = ProfileFragment.newInstance()
                 supportFragmentManager.beginTransaction().replace(R.id.fragments_frame, profileFragment).commit()
